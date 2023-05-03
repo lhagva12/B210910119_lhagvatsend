591fd7b533e3412c0bad20d6da48f7fd5f8b7826
Admin <Admin@DESKTOP-IUTTIU5>
2023-05-03 14:01:27 +0800

    Soril2

---
+++ src/Course.java
@@ -0,0 +1,14 @@
+
+public class Course {
+	String courseName;
+	Course(String courseName){
+		this.courseName = courseName;
+	}
+  public String getCourseName() {
+	  return courseName;
+  }
+  public void setCourseName(String courseName) {
+	  this.courseName = courseName;
+  }
+
+}
